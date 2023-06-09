20b194038ec314691a1a91ff480602918f27c701
BennyG117 <bengomezp@gmail.com>
2023-06-09 12:47:37 -0700

    Update

--- server.py
+++ server.py
@@ -3,47 +3,28 @@ from flask import Flask, render_template
 
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 
+#instructions when opened
+@app.route('/')
+def start_here():
+        return " Type one of the following after localhost:5001 = /play, /play/(number here)/, or /play/(number here)/(color here)"
+
+#route created for /play
 @app.route('/play')
 def index():
+    #includes defaults 
     return render_template("index3.html",x=4,color="aqua")
 
+#route created for /(x)/
 @app.route('/play/<int:num>/')
 def level_2(num):
+    #includes defaults 
     return render_template("index3.html",x=num,color="aqua")
 
+#route created for /(x)/(color)/
 @app.route('/play/<int:num>/<color>/')
 def level_3(num, color):
+    #includes defaults 
     return render_template("index3.html",x=num, color=color)
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True, port=5001)
-
-
-
-
-
-
-# @app.route('/') 
-# def hello_world():
-#     return 'Hello World!'
-# #"render_template("index.html"")  # Return the string 'Hello World!' as a response
-
-# @app.route('/dojo')
-# def dojo_name():
-#     return 'Dojo!'
-
-# @app.route('/say/<string:name>')
-# def hi_name(name):
-#     return f'Hi {name.title()}!'
-
-# @app.route('/repeat/<int:num>/<string:word>')
-# def repeating_word(num, word):
-#     repeating_word = word * num
-#     return repeating_word
-
-
-
-# @app.errorhandler(404)
-# def error_found(e):
-#     return "Sorry! No response. Try again.",404
-
